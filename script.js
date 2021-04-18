7a95d06fe674b1a63e3091a44f8a91f0839a35d3
John <nmamadaliyev@list.ru>
2021-04-18 19:03:50 +0500

    Telegram

---
+++ script.js
@@ -0,0 +1,46 @@
+function och(){
+    document.getElementsByClassName('tg-menu')[0].style.backgroundColor='#497799';
+    document.getElementsByClassName('drop-menu')[0].style.display='block';
+    document.getElementsByClassName('drop-second')[0].style.display='none';
+    document.getElementsByClassName('fa-ellipsis-v')[0].style.backgroundColor='transparent';
+}
+
+function yop(){
+    document.getElementsByClassName('tg-menu')[0].style.backgroundColor='#5682A3';
+    document.getElementsByClassName('drop-menu')[0].style.display='none';
+}
+
+function och1(){
+    document.getElementsByClassName('fa-ellipsis-v')[0].style.backgroundColor='#497799';
+    document.getElementsByClassName('drop-second')[0].style.display='block';
+    document.getElementsByClassName('drop-menu')[0].style.display='none';
+    document.getElementsByClassName('tg-menu')[0].style.backgroundColor='#5682A3';
+}
+
+function yop1(){
+    document.getElementsByClassName('fa-ellipsis-v')[0].style.backgroundColor='transparent';
+    document.getElementsByClassName('drop-second')[0].style.display='none';
+}
+
+function bek(){
+    document.getElementsByClassName('chat-list')[5].style.backgroundColor='#6490B1';
+    document.getElementById('dialog').style.color='white';
+    document.getElementById('chas').style.color='white';
+    document.getElementById('msg').style.color='white';
+    document.getElementsByClassName('first')[0].style.display='none';
+    document.getElementsByClassName('dd')[0].style.display='block';
+    document.getElementsByClassName('tg-last')[0].style.display='block';
+    document.getElementsByClassName('tg-sec')[0].style.display='flex';
+}
+
+function jap(){
+    document.getElementsByClassName('background')[0].style.display='none';
+}
+
+function o4(){
+    document.getElementsByClassName('background')[0].style.display='grid';
+}
+
+// function first(){
+    
+// }
